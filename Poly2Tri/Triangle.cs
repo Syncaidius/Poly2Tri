b304bbda146a1466b0e6f317507f0a77a02ce09c
James Yarwood <syncaidius@googlemail.com>
2023-11-05 13:39:36 +0000

    Cleanup

--- Poly2Tri/Triangle.cs
+++ Poly2Tri/Triangle.cs
@@ -1,10 +1,5 @@
-﻿using Poly2Tri;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Poly2Tri
 {
--- PolygonExample.WinForms/PolygonRenderer.cs
+++ PolygonExample.WinForms/PolygonRenderer.cs
@@ -1,13 +1,7 @@
-﻿using System;
+﻿using Poly2Tri;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Poly2Tri;
 
 namespace PolygonExample
 {
